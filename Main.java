7798da888e36ac3787dca9a791e75769612f78c8
sehyeonn <a3920679@naver.com>
2022-09-26 17:08:20 +0900

    Main.java 수정 #3, Hello.java 수정 #3

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main { 
     public static void main(String[] args) { 
-        Hello hello = new Hello();
+        Hello hello = new Hello("hello world");
         hello.say();
     }
 }
